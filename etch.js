44fb638dd712bfb93faf815e0ad185141aad48a2
Jeff <doylejeff98@gmail.com>
2022-11-11 14:11:46 -0500

    commit

--- etch.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let grid = document.querySelector(".grid-container");
-
-    grid.style.gridTemplateColumns = "repeat(15, 1fr)";
-    grid.style.gridTemplateRows = "repeat(15, 1fr)";
-    console.log("hi")
-})
