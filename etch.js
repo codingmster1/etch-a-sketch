bf8abe1ec4cf2d55528256e5621ef980eebcd014
Jeff <doylejeff98@gmail.com>
2022-11-11 14:29:41 -0500

    commit

--- etch.js
+++ etch.js
@@ -1,2 +1,14 @@
 const buttons = document.querySelectorAll('buttons');
-const grid = document.querySelector('grid');
+const grid = document.querySelector('.grid');
+
+let pixel = '';
+let gridSize = 50;
+
+const drawGrid = (screenSize) => {
+    for(i = 0; i < screenSize ** 2; i++){
+    pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    pixel.style.backgroundColor = "white";
+    screen.appendChild(pixel);
+    }
+}
